10bddd8455b0c781f132f28317c2d07c8e5ba068
Matías Beraldo Fernández <matias.beraldo.work@gmail.com>
2023-07-22 17:34:03 +0200

    Added home router

--- Views/Home.jsx
+++ Views/Home.jsx
@@ -1,20 +1,32 @@
+State.init({
+    currentView: "home"
+});
+
 let views = {
-        home: (
-          <>
-            <Widget src="mattb.near/widget/Geoquete.Components.CreateQuest" />
-          </>
-        ),
-        join: (
-          <>
-            <Widget src="mattb.near/widget/Geoquete.Components.QuestList" />
-          </>
-        ),
-      };
-      
-      return (
-        <>
-          <Widget src="mattb.near/widget/Geoquete.Components.Header" />
-          {views["join"]}
-        </>
-      );
-      
+  home: (
+    <>
+      <Widget src="mattb.near/widget/Geoquete.Components.CreateQuest" />
+    </>
+  ),
+  create: (
+    <>
+      <Widget src="mattb.near/widget/Geoquete.Components.CreateQuest" />
+    </>
+  ),
+  join: (
+    <>
+      <Widget src="mattb.near/widget/Geoquete.Components.QuestList" />
+    </>
+  ),
+};
+
+return (
+  <>
+    <Widget src="mattb.near/widget/Geoquete.Components.Header" 
+        props={{
+            onRefresh: (tab) => State.update({ currentView: tab })
+        }}
+    />
+    {state.currentView in views ? views[state.currentView] : "404"}
+  </>
+);
